4a81cc77fb68f46a9eafe139f81eda5aca7c3478
matheusmoc <matheusandrade009@gmail.com>
2025-11-20 17:13:48 -0300

    feat: create contact and contact_phone tables with foreign key constraint

--- back-end/migration/20210514-contact.sql
+++ back-end/migration/20210514-contact.sql
@@ -1,8 +1,17 @@
-CREATE TABLE `contact` ( 
-    `contactId` INT UNSIGNED NOT NULL AUTO_INCREMENT , 
-    `name` VARCHAR(100) NOT NULL , 
-    `email` VARCHAR(100) NOT NULL , 
-    `dateCreated` DATETIME NOT NULL , 
-    `dateUpdated` DATETIME NOT NULL , 
+CREATE TABLE IF NOT EXISTS `contact` (
+    `contactId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `dateCreated` DATETIME NOT NULL,
+    `dateUpdated` DATETIME NOT NULL,
     PRIMARY KEY (`contactId`)
-) ENGINE = InnoDB; 
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `contact_phone` (
+    `contactPhoneId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `contactId` INT UNSIGNED NOT NULL,
+    `phone` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`contactPhoneId`),
+    INDEX (`contactId`),
+    CONSTRAINT `fk_contact_phone_contact` FOREIGN KEY (`contactId`) REFERENCES `contact`(`contactId`) ON DELETE CASCADE
+) ENGINE=InnoDB;
